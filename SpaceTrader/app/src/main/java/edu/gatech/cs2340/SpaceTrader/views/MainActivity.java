013c988f6eca0aa1798bd957681aea2959a06b5c
Savyasachi Nemani <ram.nemani@gmail.com>
2019-02-18 23:40:44 -0500

    made changes to Splash Screen

--- SpaceTrader/app/src/main/java/edu/gatech/cs2340/SpaceTrader/views/MainActivity.java
+++ SpaceTrader/app/src/main/java/edu/gatech/cs2340/SpaceTrader/views/MainActivity.java
@@ -1,15 +1,35 @@
 package edu.gatech.cs2340.SpaceTrader.views;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
 
 import edu.gatech.cs2340.SpaceTrader.R;
 
 public class MainActivity extends AppCompatActivity {
+    //public static final int ADD_COURSE_REQUEST_ID = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Button button = (Button)findViewById(R.id.ready_player_one);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onStartPressed();
+            }
+        });
+
     }
+    public void onStartPressed(){
+        Intent intent = new Intent(MainActivity.this, CreatePlayer.class);
+        startActivity(intent);
+        //startActivity(new Intent(MainActivity.this, CreatePlayer.class));
+    }
+
 }
