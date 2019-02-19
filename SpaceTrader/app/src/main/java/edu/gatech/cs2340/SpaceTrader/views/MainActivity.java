439d8dcb9c262ae031f4a3df840e1860912941a0
jennakohls <jennakohls@gmail.com>
2019-02-19 18:16:25 -0500

    removed redundant intent change

--- SpaceTrader/app/src/main/java/edu/gatech/cs2340/SpaceTrader/views/MainActivity.java
+++ SpaceTrader/app/src/main/java/edu/gatech/cs2340/SpaceTrader/views/MainActivity.java
@@ -12,13 +12,13 @@ import edu.gatech.cs2340.SpaceTrader.R;
 
 public class MainActivity extends AppCompatActivity {
     //public static final int ADD_COURSE_REQUEST_ID = 1;
-
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
-        final Button button = (Button)findViewById(R.id.ready_player_one);
+        button = findViewById(R.id.ready_player_one);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -31,8 +31,7 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(MainActivity.this, CreatePlayer.class);
         startActivity(intent);
         //startActivity(new Intent(MainActivity.this, CreatePlayer.class));
-        setContentView(R.layout.activity_create_player);
-
+        //setContentView(R.layout.activity_create_player);
     }
 
 }
