183543db9ac9836813ec10f0a16929d47b8b0769
OReece <omreece@gmail.com>
2019-02-25 17:17:34 -0500

    Update Planet.java

--- SpaceTrader/app/src/main/java/edu/gatech/cs2340/SpaceTrader/entity/Planet.java
+++ SpaceTrader/app/src/main/java/edu/gatech/cs2340/SpaceTrader/entity/Planet.java
@@ -1,4 +1,20 @@
 package edu.gatech.cs2340.SpaceTrader.entity;
 /** This class represents a single Planet*/
 public class Planet {
+    private String name;
+    private int techLevel;
+
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setTechLevel(int techLevel){
+        this.techLevel = techLevel;
+    }
+
+    public String getName(){
+        return name;
+    }
+    public int getTechLevel(){
+        return techLevel;
+    }
 }
