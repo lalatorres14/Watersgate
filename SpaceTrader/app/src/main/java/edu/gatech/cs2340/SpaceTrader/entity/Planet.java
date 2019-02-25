ce784cf88222985c39a213d3663f92aea9023d9f
OReece <omreece@gmail.com>
2019-02-25 17:58:30 -0500

    Planet and Market
    Added a string array of planet names, created the market object, and added attributes to the planet object.

--- SpaceTrader/app/src/main/java/edu/gatech/cs2340/SpaceTrader/entity/Planet.java
+++ SpaceTrader/app/src/main/java/edu/gatech/cs2340/SpaceTrader/entity/Planet.java
@@ -1,8 +1,19 @@
 package edu.gatech.cs2340.SpaceTrader.entity;
+import java.util.Random;
 /** This class represents a single Planet*/
 public class Planet {
     private String name;
     private int techLevel;
+    private int resources;
+    private Market market;
+
+    public Planet(){
+        Random rand = new Random();
+        setName("default");
+        setResources(rand.nextInt(13));
+        setTechLevel(rand.nextInt(8));
+        setMarket(new Market());
+    }
 
     public void setName(String name){
         this.name = name;
@@ -10,11 +21,23 @@ public class Planet {
     public void setTechLevel(int techLevel){
         this.techLevel = techLevel;
     }
+    public void setResources(int resources){
+        this.resources = resources;
+    }
+    public void setMarket(Market market){
+        this.market = market;
+    }
 
     public String getName(){
         return name;
     }
     public int getTechLevel(){
         return techLevel;
+    }
+    public int getResources(){
+        return resources;
+    }
+    public Market getMarket() {
+        return market;
     }
 }
