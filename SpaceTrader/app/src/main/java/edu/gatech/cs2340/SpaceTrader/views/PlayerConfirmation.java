e139ad9526aca4fb1bb4e2e56f68db61fa6d03e5
jennakohls <jennakohls@gmail.com>
2019-02-26 14:01:20 -0500

    changed log dest

--- SpaceTrader/app/src/main/java/edu/gatech/cs2340/SpaceTrader/views/PlayerConfirmation.java
+++ SpaceTrader/app/src/main/java/edu/gatech/cs2340/SpaceTrader/views/PlayerConfirmation.java
@@ -76,10 +76,10 @@ public class PlayerConfirmation extends AppCompatActivity {
     }
     public static void largeLog(String tag, String content) {
         if (content.length() > 4000) {
-            Log.i(tag, content.substring(0, 4000));
+            Log.d(tag, content.substring(0, 4000));
             largeLog(tag, content.substring(4000));
         } else {
-            Log.i(tag, content);
+            Log.d(tag, content);
         }
     }
 }
