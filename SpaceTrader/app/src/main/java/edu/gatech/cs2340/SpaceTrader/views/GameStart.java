75d10f714efaf87c67024313e0752f41f09545ad
jennakohls <jennakohls@gmail.com>
2019-02-26 19:11:37 -0500

    gameStart has its own layout file now

--- SpaceTrader/app/src/main/java/edu/gatech/cs2340/SpaceTrader/views/GameStart.java
+++ SpaceTrader/app/src/main/java/edu/gatech/cs2340/SpaceTrader/views/GameStart.java
@@ -10,25 +10,21 @@ import edu.gatech.cs2340.SpaceTrader.R;
 
 public class GameStart extends AppCompatActivity {
 
-    private Button button;
+    //private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_main);
-        button = findViewById(R.id.ready_player_one);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                onStartPressed();
-            }
-        });
+        setContentView(R.layout.game_start);
+        //button = findViewById(R.id.ready_player_one);
+        //button.setOnClickListener(new View.OnClickListener() {
+
 
     }
-    public void onStartPressed(){
-        Intent intent = new Intent(GameStart.this, CreatePlayer.class);
-        startActivity(intent);
-        //startActivity(new Intent(MainActivity.this, CreatePlayer.class));
-        //setContentView(R.layout.activity_create_player);
-    }
+//    public void onStartPressed(){
+//        Intent intent = new Intent(GameStart.this, CreatePlayer.class);
+//        startActivity(intent);
+//        //startActivity(new Intent(MainActivity.this, CreatePlayer.class));
+//        //setContentView(R.layout.activity_create_player);
+//    }
 }
