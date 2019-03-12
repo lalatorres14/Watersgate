b5eb3e6ae486188788e77e2bce9c0a6a29cee7bd
OReece <omreece@gmail.com>
2019-03-12 16:24:29 -0400

    1st Good Market
    Market Screen now has UI for first good. Fixing layout issues before adding the rest.

--- SpaceTrader/app/src/main/java/edu/gatech/cs2340/SpaceTrader/views/InMarket.java
+++ SpaceTrader/app/src/main/java/edu/gatech/cs2340/SpaceTrader/views/InMarket.java
@@ -2,8 +2,12 @@ package edu.gatech.cs2340.SpaceTrader.views;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 import edu.gatech.cs2340.SpaceTrader.R;
+import edu.gatech.cs2340.SpaceTrader.entity.Game;
+import edu.gatech.cs2340.SpaceTrader.entity.Planet;
+import edu.gatech.cs2340.SpaceTrader.entity.Player;
 
 public class InMarket extends AppCompatActivity {
 
@@ -11,6 +15,11 @@ public class InMarket extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.market_screen);
+
+        Planet current = Game.getInstance().getUniverse().get(0).getPlanetList().get(0);
+        Player player = Game.getInstance().getPlayer();
+        final TextView nameTextView = findViewById(R.id.marketHeader);
+        nameTextView.setText(current.getName() + "'s Bazaar");
     }
 
 }
