2632ea23f5ef40a223fa83eee845a48f310a8e6d
jennakohls <jennakohls@gmail.com>
2019-03-13 13:21:21 -0400

    adds planet screen, also makes techLevel and Resource enums/simplifies random generation
    sorry i should have separated this in 2 commits rip

---
+++ SpaceTrader/app/src/main/java/edu/gatech/cs2340/SpaceTrader/entity/Resource.java
@@ -0,0 +1,33 @@
+package edu.gatech.cs2340.SpaceTrader.entity;
+
+import java.util.Random;
+
+public enum Resource {
+    NOSPECIALRESOURCES ("No Special Resources"),
+    MINERALRICH ("Mineral Rich"),
+    MINERALPOOR ("Mineral Poor"),
+    LOTSOFWATER ("Lots of Water"),
+    RICHSOIL ("Rich Soil"),
+    POORSOIL ("Poor Soil"),
+    RICHFAUNA ("Rich Fauna"),
+    LIFELESS ("Lifeless"),
+    WEIRDMUSHROOMS ("Weird Mushrooms"),
+    LOTSOFHERBS ("Lots of Herbs"),
+    ARTISTIC ("Artistic"),
+    WARLIKE ("Warlike");
+
+    private String formattedName;
+
+    Resource(String f){
+        formattedName = f;
+    }
+
+    @Override public String toString(){
+        return formattedName;
+    }
+
+    public static Resource getRandomResource() {
+        Random random = new Random();
+        return values()[random.nextInt(values().length)];
+    }
+}
--- SpaceTrader/app/src/main/java/edu/gatech/cs2340/SpaceTrader/views/PlanetScreen.java
+++ SpaceTrader/app/src/main/java/edu/gatech/cs2340/SpaceTrader/views/PlanetScreen.java
@@ -9,6 +9,7 @@ import android.widget.TextView;
 
 import edu.gatech.cs2340.SpaceTrader.R;
 import edu.gatech.cs2340.SpaceTrader.entity.Game;
+import edu.gatech.cs2340.SpaceTrader.entity.Planet;
 
 public class PlanetScreen extends AppCompatActivity {
     //public static final int ADD_COURSE_REQUEST_ID = 1;
@@ -25,11 +26,20 @@ public class PlanetScreen extends AppCompatActivity {
                 onStartPressed();
             }
         });
-
-        final TextView nameTextView = findViewById(R.id.nameText);
+        Planet p = Game.getInstance().getUniverse().get(0).getPlanetList().get(0);
+        final TextView nameTextView = findViewById(R.id.planetText);
         //so this is bad!! once travel is a thing, doing so needs to somehow set this text here and
         // set a general 'current planet' variable
-        nameTextView.setText(Game.getInstance().getUniverse().get(0).getPlanetList().get(0).getName());
+        nameTextView.setText(p.getName());
+
+        final TextView solarSystemTextView = findViewById(R.id.solarSystemText);
+        solarSystemTextView.setText(String.format("Solar System %s",Game.getInstance().getUniverse().get(0).getName()));
+
+        final TextView rssTextView = findViewById(R.id.rssText);
+        rssTextView.setText(String.format("Resources: %s",p.getResources().toString()));
+
+        final TextView techLevelTextView = findViewById(R.id.techLevelText);
+        techLevelTextView.setText(String.format("Tech Level: %s",p.getTechLevel()));
 
     }
     public void onStartPressed(){
